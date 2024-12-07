89c81da782c6d4d21548bef6c7380e5551c571dd
Michael Bloom <mike.r.bloom@protonmail.com>
2024-12-07 11:42:51 -0500

    Part2 working

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -8,18 +8,20 @@
 #include <algorithm>
 #include <cmath>
 
-std::vector<std::vector<long int>> read_file()
+using num_type = long;
+
+std::vector<std::vector<num_type>> read_file()
 {
     std::ifstream infile("input.txt");
-    std::vector<std::vector<long int>> csv_rows;
+    std::vector<std::vector<num_type>> csv_rows;
 
     for (std::string line; std::getline(infile, line);) {
         std::istringstream ss(line);
-        std::vector<long int> row;
+        std::vector<num_type> row;
 
-        long int result{ 0 };
+        num_type result{ 0 };
         char colon{ 0 };
-        long int operand{ 0 };
+        num_type operand{ 0 };
         ss >> result >> colon;
         row.push_back(result);
         while (ss >> operand) {
@@ -32,9 +34,9 @@ std::vector<std::vector<long int>> read_file()
     return csv_rows;
 }
 
-enum class oper : unsigned int { add = 0, multiply = 1, concat };
+enum class oper : unsigned int { add = 0, multiply = 1, concat = 2 };
 
-long int do_oper(oper oper_in, long int a, long int b)
+num_type do_oper(oper oper_in, num_type a, num_type b)
 {
     switch (oper_in) {
     case oper::add:
@@ -49,11 +51,13 @@ long int do_oper(oper oper_in, long int a, long int b)
         //find the log10 of b
         double log_b = std::log10(static_cast<double>(b));
         //round it up
-        double round_up = std::ceil(log_b);
+        //double round_up = std::ceil(log_b);
+        double round_up = std::floor(log_b) + 1.0;
         //compute the scale for a
         double scale = std::pow(10, round_up);
-        long lscale = std::lround(scale);
-        return a * lscale + b;
+        num_type lscale = std::lround(scale);
+        num_type value = a * lscale + b;
+        return value;
     } break;
 
     default:
@@ -75,9 +79,12 @@ struct oper_permutator {
                 break;
             } else if (opers.at(i) == oper::multiply) {
                 opers.at(i) = oper::concat;
+                //opers.at(i) = oper::add;
                 break;
             } else if (opers.at(i) == oper::concat) {
                 opers.at(i) = oper::add;
+            } else {
+                throw std::runtime_error("Error doing permutation!");
             }
         }
         return !std::all_of(opers.begin(), std::next(opers.begin(), num_of_operators),
@@ -94,22 +101,28 @@ int main()
 {
     const auto equations = read_file();
 
-    long int result_sum{ 0 };
+    num_type result_sum{ 0 };
 
     for (const auto &equation : equations) {
         oper_permutator permu{ .num_of_operators = equation.size() - 2 };
 
+        //std::cout << "halp\n";
+
         do {
-            long int result = equation.at(1);
+            num_type result = equation.at(1);
             for (size_t i = 2; i < equation.size(); i++) {
                 oper oper_to_do = permu.get_oper(i - 2);
                 result = do_oper(oper_to_do, result, equation.at(i));
+                if (result < 0)
+                    std::cout << "here\n";
             }
             if (result == equation.at(0)) {
                 result_sum += result;
                 break;
             }
         } while (permu.next_permutation());
+
+        //std::cout << "halp\n";
     }
 
     std::cout << result_sum << '\n';
