f5f62de8758c29beb4fc9d303c90a9c26913ea0d
Michael Bloom <mike.r.bloom@protonmail.com>
2024-12-15 17:27:31 -0500

    Refactor for part 2

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <ranges>
 #include <iostream>
+#include <algorithm>
 
 template <typename T, typename U, typename V, typename W>
 auto operator+(const std::pair<T, U> &l, const std::pair<V, W> &r)
@@ -29,11 +30,14 @@ using xy_pos_t = std::pair<int, int>;
 
 struct cell {
     bool has_wall{ false };
-    bool has_box{ false };
+};
 
-    bool is_empty()
+struct box {
+    xy_pos_t pos1{ 0, 0 };
+    xy_pos_t pos2{ 0, 0 };
+
+    box(xy_pos_t pos) : pos1(pos), pos2(pos)
     {
-        return !has_wall && !has_box;
     }
 };
 
@@ -44,13 +48,15 @@ struct robot {
 using warehouse_row_t = std::vector<cell>;
 using warehouse_map_t = std::vector<warehouse_row_t>;
 using move_list_t = std::vector<xy_pos_t>;
+using box_list_t = std::vector<box>;
 
-std::tuple<warehouse_map_t, robot, move_list_t> read_file()
+std::tuple<warehouse_map_t, box_list_t, robot, move_list_t> read_file()
 {
     std::ifstream infile("input.txt");
     warehouse_map_t csv_rows;
     robot rob;
     move_list_t moves;
+    box_list_t boxes;
     int y{ 0 };
 
     for (std::string line; std::getline(infile, line);) {
@@ -67,7 +73,8 @@ std::tuple<warehouse_map_t, robot, move_list_t> read_file()
                 row.push_back(cell{ .has_wall = true });
                 break;
             case 'O':
-                row.push_back(cell{ .has_box = true });
+                row.push_back(cell{});
+                boxes.push_back(box(xy_pos_t{ x, y }));
                 break;
             case '.':
                 row.push_back(cell{});
@@ -109,31 +116,60 @@ std::tuple<warehouse_map_t, robot, move_list_t> read_file()
         }
     }
 
-    return { csv_rows, rob, moves };
+    return { csv_rows, boxes, rob, moves };
 }
 
-void execute_move(warehouse_map_t &map, robot &rob, xy_pos_t move)
+bool peek_box_push(warehouse_map_t &map, box_list_t &boxes, box &b, xy_pos_t move)
+{
+    xy_pos_t pos1_push = b.pos1 + move;
+    if (map.at(pos1_push.second).at(pos1_push.first).has_wall)
+        return false;
+    //see if there is any boxes we push into
+    auto itr = std::find_if(boxes.begin(), boxes.end(), [pos1_push](auto &a) { return a.pos1 == pos1_push; });
+    if (itr == boxes.end())
+        return true;
+    else
+        return peek_box_push(map, boxes, (*itr), move);
+}
+
+void do_box_push(warehouse_map_t &map, box_list_t &boxes, box &b, xy_pos_t move)
+{
+    xy_pos_t pos1_push = b.pos1 + move;
+    
+    //see if there is any boxes we push into
+    auto itr = std::find_if(boxes.begin(), boxes.end(), [pos1_push](auto &a) { return a.pos1 == pos1_push; });
+    if (itr != boxes.end())
+        do_box_push(map, boxes, (*itr), move);
+
+    b.pos1 = b.pos1 + move;
+    b.pos2 = b.pos2 + move;
+}
+
+void execute_move(warehouse_map_t &map, box_list_t &boxes, robot &rob, xy_pos_t move)
 {
     //look in the direction of the move until we find either open space or wall
     xy_pos_t look_ahead = rob.pos + move;
     cell look_ahead_cell = map.at(look_ahead.second).at(look_ahead.first);
-    while (!look_ahead_cell.has_wall && !look_ahead_cell.is_empty()) {
-        look_ahead = look_ahead + move;
-        look_ahead_cell = map.at(look_ahead.second).at(look_ahead.first);
-    }
-    //if we find a wall we can't execute the move
+
     if (look_ahead_cell.has_wall)
         return;
-    //if we find an open space then we can push all boxes
-    while (look_ahead != rob.pos + move) {
-        map.at(look_ahead.second).at(look_ahead.first).has_box = true;
-        look_ahead = look_ahead - move;
+
+    auto itr = std::find_if(boxes.begin(), boxes.end(), [look_ahead](auto &a) { return a.pos1 == look_ahead; });
+
+    //no boxes so just move into empty space
+    if (itr == boxes.end()) {
+        rob.pos = look_ahead;
+        return;
     }
-    map.at(look_ahead.second).at(look_ahead.first).has_box = false;
-    rob.pos = look_ahead;
+
+    //check if boxes can be pushed
+    if (peek_box_push(map, boxes, (*itr), move)) {
+        do_box_push(map, boxes, (*itr), move);
+        rob.pos = look_ahead;
+    }
 }
 
-void print_map(warehouse_map_t &map, robot &rob, int iteration, xy_pos_t move)
+void print_map(const warehouse_map_t &map, const box_list_t &boxes, const robot &rob, int iteration, xy_pos_t move)
 {
     std::ofstream out("output.txt", std::ios_base::app);
     out << iteration << " " << move.first << " " << move.second << '\n';
@@ -145,10 +181,17 @@ void print_map(warehouse_map_t &map, robot &rob, int iteration, xy_pos_t move)
         for (const auto &cell : row) {
             if (cell.has_wall)
                 print_output.back().push_back('#');
-            else if (cell.has_box)
-                print_output.back().push_back('O');
             else
                 print_output.back().push_back('.');
+        }
+    }
+
+    for (const auto &box : boxes) {
+        if (box.pos1 == box.pos2) {
+            print_output.at(box.pos1.second).at(box.pos1.first) = 'O';
+        } else {
+            print_output.at(box.pos1.second).at(box.pos1.first) = '[';
+            print_output.at(box.pos2.second).at(box.pos2.first) = ']';
         }
     }
 
@@ -159,31 +202,27 @@ void print_map(warehouse_map_t &map, robot &rob, int iteration, xy_pos_t move)
     }
 }
 
-long score_map(const warehouse_map_t & map)
+long score_map(const box_list_t & boxes)
 {
-    long score {0};
-    for (size_t y : std::views::iota(0u, map.size())) {
-        for (size_t x : std::views::iota(0u, map.at(y).size())) {
-            if (map.at(y).at(x).has_box)
-                score += y * 100 + x;
-        }
-    }
+    long score{ 0 };
+    for (const auto & box : boxes)
+        score += box.pos1.second * 100 + box.pos1.first;
 
     return score;
 }
 
 int main()
 {
-    auto [map, rob, moves] = read_file();
+    auto [map, boxes, rob, moves] = read_file();
 
     int iteration{ 0 };
-    //print_map(map, rob, iteration, xy_pos_t{ 0, 0 });
+    //print_map(map, boxes, rob, iteration, xy_pos_t{ 0, 0 });
     for (const auto &move : moves) {
         iteration++;
-        execute_move(map, rob, move);
-        //print_map(map, rob, iteration, move);
+        execute_move(map, boxes, rob, move);
+        //print_map(map, boxes, rob, iteration, move);
     }
 
-    long score = score_map(map);
+    long score = score_map(boxes);
     std::cout << score << '\n';
 }
