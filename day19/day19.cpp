3367ce315a85998d538acd59dba943c02be2bc3e
Michael Bloom <mike.r.bloom@protonmail.com>
2025-01-04 23:09:40 +0000

    Cleanup

--- day19/day19.cpp
+++ day19/day19.cpp
@@ -37,33 +37,16 @@ std::tuple<string_row_t, string_row_t> read_file()
     return { towels, designs };
 }
 
-bool remake_design(const std::string_view design, const string_row_t &towels, const std::vector<size_t> &contains_index)
+long build_design(const std::string_view design, const string_row_t &towels, const std::vector<size_t> &contains_index,
+                  std::unordered_map<std::string, long> &umap)
 {
-    for (size_t i = 0; i < contains_index.size(); i++) {
-        std::string_view towel = towels.at(contains_index.at(i));
-        if (design.starts_with(towel)) {
-            if (design.size() == towel.size()) {
-                //we are done with the design
-                return true;
-            } else {
-                //new subview
-                std::string_view sub_design = design.substr(towel.size());
-                bool check = remake_design(sub_design, towels, contains_index);
-                if (check)
-                    return check;
-            }
-        }
+    if (umap.contains(std::string(design))) {
+        return umap[std::string(design)];
     }
 
-    return false;
-}
-
-long remake_design_part2(const std::string_view design, const string_row_t &towels,
-                         const std::vector<size_t> &contains_index, std::unordered_map<std::string, long> &umap)
-{
     long result{ 0 };
-    for (size_t i = 0; i < contains_index.size(); i++) {
-        std::string_view towel = towels.at(contains_index.at(i));
+    for (const auto i : contains_index) {
+        std::string_view towel = towels.at(i);
         if (design.starts_with(towel)) {
             if (design.size() == towel.size()) {
                 //we are done with the design
@@ -71,16 +54,12 @@ long remake_design_part2(const std::string_view design, const string_row_t &towe
             } else {
                 //new subview
                 std::string_view sub_design = design.substr(towel.size());
-                if (umap.contains(std::string(sub_design))) {
-                    result += umap[std::string(sub_design)];
-                } else {
-                    long temp_result = remake_design_part2(sub_design, towels, contains_index, umap);
-                    umap[std::string(sub_design)] = temp_result;
-                    result += temp_result;
-                }
+                result += build_design(sub_design, towels, contains_index, umap);
             }
         }
     }
+
+    umap[std::string(design)] = result;
 
     return result;
 }
@@ -89,30 +68,27 @@ int main()
 {
     auto [towels, designs] = read_file();
 
-    long result{ 0 };
+    long result_part1{ 0 };
     long result_part2{ 0 };
     std::unordered_map<std::string, long> umap;
 
     for (const auto &design : designs) {
         std::vector<size_t> contains_index;
 
-        for (size_t i = 0; i < towels.size(); i++) {
-            if (string_contains(design, towels.at(i)))
+        size_t i{ 0 };
+        for (const auto &towel : towels) {
+            if (string_contains(design, towel))
                 contains_index.push_back(i);
+            i++;
         }
 
-        if (remake_design(design, towels, contains_index))
-            result++;
-
-        if (umap.contains(design)) {
-            result_part2 += umap[design];
-        } else {
-            long temp_result = remake_design_part2(design, towels, contains_index, umap);
-            umap[design] = temp_result;
+        long temp_result = build_design(design, towels, contains_index, umap);
+        if (temp_result > 0) {
+            result_part1++;
             result_part2 += temp_result;
         }
     }
 
-    std::cout << result << '\n';
+    std::cout << result_part1 << '\n';
     std::cout << result_part2 << '\n';
 }
